f7b582cfaab353565d7e4e6133c9a705ceb8ea06
Mahovplus <mahovplus66@mail.ru>
2024-09-05 01:35:24 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+home_work_completed = '12'
+lost_time = '1.5'
+name_shool = 'Python'
+time_on_task = '0.125'
+print('Курс:', name_shool, '. Всего задач:', home_work_completed, '. Затрачено часов:', lost_time, '. Среднее время выполнения:', time_on_task)
